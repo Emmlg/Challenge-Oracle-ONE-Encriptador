5120099e30411147b22523b5bb04e0e7430801e9
Emmanuel <l21700198@comitan.tecnm.mx>
2023-05-28 22:31:49 -0600

    proyecto terminado, falta validar con responsividad

--- logica.js
+++ logica.js
@@ -1,59 +1,79 @@
 
  function EncriptarTexto(){
 
-    let texto = document.getElementById("textEncriptador");
-    var newtx = texto.value;
-    
+    var textArea = document.getElementById("textarea").value;
 
-var textencriptado = newtx.replace(/[aeiou]/gi, function(letra) {
-  switch (letra.toLowerCase()) {
-    case 'a':
-      return 'ai';
-    case 'e':
-      return 'enter';
-    case 'i':
-      return 'imes';
-    case 'o':
-      return 'ober';
-    case 'u':
-      return 'ufat';
-    default:
-      return letra;
-  }
-});
+    if(textArea != ""){
 
-textdesencriptador.value = textencriptado;
+      const newtx = textArea;
+      console.log(newtx);
+  
+      var textencriptado = newtx.replace(/[aeiou]/gi, function(letra) {
+        switch (letra.toLowerCase()) {
+          case 'a':
+            return 'ai';
+          case 'e':
+            return 'enter';
+          case 'i':
+            return 'imes';
+          case 'o':
+            return 'ober';
+          case 'u':
+            return 'ufat';
+          default:
+            return letra;
+        }
+      });
+      
+      console.log(textencriptado);
+      textarea.value ="";
+      mensaje.style.backgroundImage ="none";
+      mensaje.value = textencriptado; 
 
-
+    }
 
 }
 
 
 function DescriptarTxto(){
 
-    let texto = document.getElementById("textdesencriptador");
-    var newtx = texto.value;
+    let mensage = document.getElementById("mensaje").value;
+
+    if(mensage !=""){
+
+      var newtx = mensage;
     
 
-var txDescriptar = newtx.replace(/ai|enter|imes|ober|ufat/gi, function(letra) {
-  switch (letra.toLowerCase()) {
-    case 'ai':
-      return 'a';
-    case 'enter':
-      return 'e';
-    case 'imes':
-      return 'i';
-    case 'ober':
-      return 'o';
-    case 'ufat':
-      return 'u';
-    default:
-      return letra;
-  }
-});
+      var txDescriptar = newtx.replace(/ai|enter|imes|ober|ufat/gi, function(letra) {
+        switch (letra.toLowerCase()) {
+          case 'ai':
+            return 'a';
+          case 'enter':
+            return 'e';
+          case 'imes':
+            return 'i';
+          case 'ober':
+            return 'o';
+          case 'ufat':
+            return 'u';
+          default:
+            return letra;
+        }
+      });
+      
+      mensaje.value = "";
+      mensaje.style.backgroundImage = "url('img/Muñeco.png')";
+      textarea.value = txDescriptar;
 
-textEncriptador.value = txDescriptar;
+    }
 
+
+}
+
+function copiarCliboard(){
+
+  let mensage = document.getElementById("mensaje").value;
+  navigator.clipboard.writeText(mensage);
 
 
 }
